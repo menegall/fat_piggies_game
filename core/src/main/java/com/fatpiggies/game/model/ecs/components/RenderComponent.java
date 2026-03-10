8d3d2b7f2b40478238d274da8f35b650e64f5874
Ingvild Sandven <ingviksa@stud.ntnu.no>
2026-03-10 19:59:49 +0100

    feat: added ecs components: Modifiers, Render component, Collider component (#5)

---
+++ core/src/main/java/com/fatpiggies/game/model/ecs/components/RenderComponent.java
@@ -0,0 +1,5 @@
+package com.fatpiggies.game.model.ecs.components;
+
+public class RenderComponent {
+    public String textureId;
+}
---
+++ core/src/main/java/com/fatpiggies/game/model/ecs/components/VelocityModifierComponent.java
@@ -0,0 +1,5 @@
+package com.fatpiggies.game.model.ecs.components;
+
+public class VelocityModifierComponent {
+    public int power;
+}
