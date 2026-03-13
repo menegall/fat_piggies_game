81d57fd0823e52035e3fcfe29ca3170546af7f7d
Patrick Menegalli <menegallipatrick@gmail.com>
2026-03-13 17:41:31 +0100

    Removed wrong file

---
+++ core/src/main/java/com/fatpiggies/game/model/ecs/systems/NetworkLerpSystem.java
@@ -0,0 +1,10 @@
+package com.fatpiggies.game.model.ecs.systems;
+
+/**
+ * Used exclusively for remote entities (other players or synchronized
+ * objects). It smoothly interpolates the current TransformComponent toward the target
+ * coordinates stored in the NetworkSyncComponent received from the database.
+ */
+public class NetworkLerpSystem {
+
+}
