ca9d9641749128165862901e9d558f14b87b9a6c
Ingvild Sandven <ingviksa@stud.ntnu.no>
2026-03-17 09:11:17 +0100

    feature: Added Menu controller render, dispose and create functions.

---
+++ core/src/main/java/com/fatpiggies/game/controller/MenuController.java
@@ -0,0 +1,29 @@
+package com.fatpiggies.game.controller;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.fatpiggies.game.view.states.GameStateManager;
+import com.fatpiggies.game.view.states.LobbyState;
+import com.fatpiggies.game.view.states.State;
+
+public class MenuController {
+
+    private GameStateManager gsm;
+    private SpriteBatch batch;
+
+    public void create(boolean isHost){
+        batch = new SpriteBatch();
+        gsm = new GameStateManager();
+        gsm.set(new LobbyState(isHost));
+
+    }
+
+    public void render(){
+        gsm.render(batch);
+    }
+
+    public void dispose(){
+        batch.dispose();
+    }
+
+
+}
