76c52f9bb587ed5eee05ba2ebdf12dcd1103df91
Paula <paulalorenzxx@gmail.com>
2026-03-17 16:27:00 +0100

    Added MainController, interface for PlayController and instance of PlayController.

--- core/src/main/java/com/fatpiggies/game/controller/MainController.java
+++ core/src/main/java/com/fatpiggies/game/controller/MainController.java
@@ -1,4 +1,25 @@
 package com.fatpiggies.game.controller;
 
+import com.badlogic.gdx.Gdx;
+import com.fatpiggies.game.view.GameStateManager;
+
 public class MainController {
+
+    public GameWorld world;
+    public PlayController playController;
+    public MenuController menuController;
+    public GameStateManager gsm;
+    // TODO: add AuthService and DBService into constructor, when firebase is merged
+    public MainController() {
+        playController = new PlayController(this);
+        menuController = new MenuController(this);
+        gsm = new GameStateManager();
+        world = new GameWorld();
+    }
+
+
+    public void update(float dt) {
+        // buffer? @patrick
+
+    }
 }
