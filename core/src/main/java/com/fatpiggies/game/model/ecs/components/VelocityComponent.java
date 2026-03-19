81065c7b61d13ff0902165677a65e64cfebe5e17
Liangchen Liu <liangchenliul@gmail.com>
2026-03-19 16:36:01 +0100

    MOVEMENT SYSTEM - Change to float VelocityComponent

--- core/src/main/java/com/fatpiggies/game/model/ecs/components/VelocityComponent.java
+++ core/src/main/java/com/fatpiggies/game/model/ecs/components/VelocityComponent.java
@@ -3,8 +3,8 @@ package com.fatpiggies.game.model.ecs.components;
 import com.badlogic.ashley.core.Component;
 
 public class VelocityComponent implements Component {
-    public double baseMaxVelocity;
-    public double currentMaxVelocity;
-    public double vx;
-    public double vy;
+    public float baseMaxVelocity;
+    public float currentMaxVelocity;
+    public float vx;
+    public float vy;
 }
