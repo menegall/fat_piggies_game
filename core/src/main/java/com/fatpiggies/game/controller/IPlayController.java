15f09ee192aee0cd2ae9dd8e75367b0f78a97b25
Paula <paulalorenzxx@gmail.com>
2026-03-19 09:55:42 +0100

    Added interface for PlayController and created two instances of it for client and host. Integrated into MainController.

--- core/src/main/java/com/fatpiggies/game/controller/IPlayController.java
+++ core/src/main/java/com/fatpiggies/game/controller/IPlayController.java
@@ -1,6 +1,7 @@
 package com.fatpiggies.game.controller;
 
 public interface IPlayController {
+    void startGame(String lobbyId);
+    void endGame(String lobbyId);
     void movePig(int x, int y);
-    void dash();
 }
