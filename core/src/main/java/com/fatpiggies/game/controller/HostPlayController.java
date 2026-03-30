e0aecc509949de7034155608d5113c105c82018d
Ingvild Sandven <ingviksa@stud.ntnu.no>
2026-03-30 11:34:27 +0200

    feat: Added logic for play controllers (#20) Co-authored by @gegiske and @piapir

--- core/src/main/java/com/fatpiggies/game/controller/HostPlayController.java
+++ core/src/main/java/com/fatpiggies/game/controller/HostPlayController.java
@@ -1,6 +1,9 @@
 package com.fatpiggies.game.controller;
 
+import com.fatpiggies.game.model.GameWorld;
 import com.fatpiggies.game.network.dto.GameState;
+import com.fatpiggies.game.view.states.GameOverState;
+import com.fatpiggies.game.view.states.PlayState;
 
 public class HostPlayController implements IPlayController {
     private MainController main;
@@ -13,9 +16,13 @@ public class HostPlayController implements IPlayController {
     public void startGame(String lobbyId) {
         // TODO specify information that is available here
         main.world = new GameWorld();
+
         main.dbs.startGame(lobbyId);
         main.dbs.pushGameState(lobbyId, new GameState());
+
         main.world.startWorld();
+
+        main.gsm.set(new PlayState());
         main.gsm.setPlayScreen();
     }
 
@@ -23,13 +30,15 @@ public class HostPlayController implements IPlayController {
     public void endGame(String lobbyId) {
         main.dbs.endGame(lobbyId);
         // TODO: determine winner
+        main.gsm.set(new GameOverState());
         main.gsm.setOverScreen();
         main.world = null;
 
     }
 
     @Override
-    public void movePig(int x, int y) {
-        // TODO
+    public void movePig(double x, double y) {
+
     }
+
 }
--- core/src/main/java/com/fatpiggies/game/controller/IPlayController.java
+++ core/src/main/java/com/fatpiggies/game/controller/IPlayController.java
@@ -3,5 +3,5 @@ package com.fatpiggies.game.controller;
 public interface IPlayController {
     void startGame(String lobbyId);
     void endGame(String lobbyId);
-    void movePig(int x, int y);
+    void movePig(double x, double y);
 }
